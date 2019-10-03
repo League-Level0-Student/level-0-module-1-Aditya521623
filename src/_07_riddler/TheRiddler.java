070903a39dc857030c78c3c837042c62b31e693c
League Student <league@iMac-18.attlocal.net>
2019-10-02 18:29:01 -0700

    badgers ip

--- src/_07_riddler/TheRiddler.java
+++ src/_07_riddler/TheRiddler.java
@@ -2,23 +2,55 @@ package _07_riddler;
 //    Copyright (c) The League of Amazing Programmers 2013-2019
 //    Level 0
 
-
+import javax.swing.JOptionPane;
 
 public class TheRiddler {
 
 	public static void main(String[] args) {
 
 		// 1. Make a variable to hold the score
+		
+		int score = 0;
 
 		// 3. Ask the user a riddle. Here are some ideas: bit.ly/some-riddles
 
+		String answer = JOptionPane.showInputDialog("What is greater than god, more evil than the devil, the poor have it, the rich need it, and is you eat it, you'll die.");		
+		
 		// 4. If they got the answer right, pop up "correct!" and increase the score by one
 
-		// 5. Otherwise, say "wrong" and tell them the answer
-
+		if( answer.equalsIgnoreCase("nothing")) {
+			
+			JOptionPane.showMessageDialog( null, " correct! " );
+			
+			score++;
+			
+		}
+		else
+		{
+		JOptionPane.showMessageDialog( null, " W-R-O-N-G!!!!!! "  );
+		}
+		
+		String answer2 = JOptionPane.showInputDialog(" You can drop me from the tallest building, and I be fine. If you drop me in water, i'll die. WHO AM I?");		
+		
 		// 6. Add some more riddles
 
-		// 2. Make a pop up to show the score.
+if( answer2.equalsIgnoreCase("paper")) {
+			
+			JOptionPane.showMessageDialog( null, " correct! " );
+			
+			 score++;
+			
+		}
+		else
+		{
+		JOptionPane.showMessageDialog( null, " W-R-O-N-G!!!!!! "  );
+		}
+
+
+
+JOptionPane.showMessageDialog( null, " Your final score is:" + score);
+
+
 		
 	}
 }
---
+++ src/_12_badgers/Argh.java
@@ -0,0 +1,19 @@
+package _12_badgers;
+
+public class Argh {
+
+	public static void main(String[] args) {
+	for(int j = 0; j<2; )	
+		for(int i = 0; i<12; i++) {
+			System.out.println("Badger ");
+				
+			
+			}
+		
+		for(int i2 = 0; i2<2; i2++) {
+			System.out.println("Mushroom ");
+
+	}
+
+}
+}
